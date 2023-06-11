a8920d0845b535e4a9148adec7cf718d62e7331c
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-11 17:02:10 -0300

    Update questao_1.c

--- Atividade (avaliativa) 12-05-2023/Questao_1/questao_1.c
+++ Atividade (avaliativa) 12-05-2023/Questao_1/questao_1.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "Cubo.h"
+
+Cubo criar_cubo(float comprimento, float largura, float altura) {
+    Cubo cubo;
+    cubo.comprimento = comprimento;
+    cubo.largura = largura;
+    cubo.altura = altura;
+    return cubo;
+}
+
+
+float obter_comprimento(Cubo cubo) {
+    return cubo.comprimento;
+}
+
+float obter_largura(Cubo cubo) {
+    return cubo.largura;
+}
+
+float obter_altura(Cubo cubo) {
+    return cubo.altura;
+}
+
+float calcular_area(Cubo cubo) {
+    float area_total = 2 * (cubo.comprimento * cubo.largura + cubo.largura * cubo.altura + cubo.altura * cubo.comprimento);
+    return area_total;
+}
+
+float calcular_volume(Cubo cubo) {
+    float volume = cubo.comprimento * cubo.largura * cubo.altura;
+    return volume;
+}
