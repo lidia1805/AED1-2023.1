c6f59ecae89ab0b6be2bfbe47125cf11111a3cfe
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-11 17:00:22 -0300

    Update questao_1.h

--- Atividade (avaliativa) 12-05-2023/Questao_1/questao_1.h
+++ Atividade (avaliativa) 12-05-2023/Questao_1/questao_1.h
@@ -1,1 +1,15 @@
+#ifndef CUBO_H_INCLUDE
+#define CUBO_H_INCLUDE
 
+typedef struct {
+    float comprimento;
+    float largura;
+    float altura;
+} Cubo;
+
+Cubo criar_cubo(float comprimento, float largura, float altura);
+float obter_comprimento(Cubo cubo);
+float obter_largura(Cubo cubo);
+float obter_altura(Cubo cubo;
+float calcular_area(Cubo cubo);
+float calcular_volume(Cubo cubo);
