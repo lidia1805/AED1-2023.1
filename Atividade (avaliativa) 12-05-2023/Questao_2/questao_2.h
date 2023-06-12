999d086162fea4722f839d439be8e8f70a448a59
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 11:38:57 -0300

    Update questao_2.h

--- Atividade (avaliativa) 12-05-2023/Questao_2/questao_2.h
+++ Atividade (avaliativa) 12-05-2023/Questao_2/questao_2.h
@@ -1,1 +1,19 @@
+#ifndef COMPLEXO_H_INCLUDE
+#define COMPLEXO_H_INCLUDE
 
+typedef struct {
+    float parte_real;
+    float parte_imaginaria;
+} NumeroComplexo;
+
+NumeroComplexo criar_complexo(float parte_real, float parte_imaginaria);
+
+void destruir_complexo(NumeroComplexo complexo) ;
+
+NumeroComplexo somar_complexos(NumeroComplexo complexo1, NumeroComplexo complexo2);
+
+NumeroComplexo subtrair_complexos(NumeroComplexo complexo1, NumeroComplexo complexo2);
+
+NumeroComplexo multiplicar_complexos(NumeroComplexo complexo1, NumeroComplexo complexo2);
+
+NumeroComplexo dividir_complexos(NumeroComplexo complexo1, NumeroComplexo complexo2);
