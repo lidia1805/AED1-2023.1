cae7b60f540881454e98e069cd516695c9aaa581
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 00:36:30 -0300

    Update main.c

--- Atividade (avaliativa) 12-05-2023/Questao_1/main.c
+++ Atividade (avaliativa) 12-05-2023/Questao_1/main.c
@@ -1,1 +1,11 @@
+int main() {
+    Cubo cubo = criar_cubo(3.5, 2.0, 4.8);
 
+    printf("Comprimento do cubo: %.2f\n", obter_comprimento(cubo));
+    printf("Largura do cubo: %.2f\n", obter_largura(cubo));
+    printf("Altura do cubo: %.2f\n", obter_altura(cubo));
+    printf("Área total do cubo: %.2f\n", calcular_area(cubo));
+    printf("Volume do cubo: %.2f\n", calcular_volume(cubo));
+
+    return 0;
+}
