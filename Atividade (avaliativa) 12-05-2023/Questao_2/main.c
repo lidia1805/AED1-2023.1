f7b24a25e873aa4e038e9fdc76c29cfb1d6c37e1
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 11:50:13 -0300

    Update main.c

--- Atividade (avaliativa) 12-05-2023/Questao_2/main.c
+++ Atividade (avaliativa) 12-05-2023/Questao_2/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "questao_2.h"
+
+int main(){
+NumeroComplexo complexo1 = criar_complexo(2.0, 3.0);
+    NumeroComplexo complexo2 = criar_complexo(1.0, 4.0);
+
+    NumeroComplexo soma = somar_complexos(complexo1, complexo2);
+    printf("Soma: %.2f + %.2fi\n", soma.parte_real, soma.parte_imaginaria);
+
+    NumeroComplexo subtracao = subtrair_complexos(complexo1, complexo2);
+    printf("Subtração: %.2f + %.2fi\n", subtracao.parte_real, subtracao.parte_imaginaria);
+}
