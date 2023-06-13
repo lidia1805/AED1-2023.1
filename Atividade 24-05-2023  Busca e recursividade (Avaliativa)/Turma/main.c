4d325de9ae2ce2ab4b072cd99de70b8c6f35675d
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 21:15:47 -0300

    Update main.c

--- Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Turma/main.c
+++ Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Turma/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "turma.h"
 
+int main() {
+   
+    TURMA turma = criar_turma(2);
+    inserir_aluno(&turma, aluno1);
+    inserir_aluno(&turma, aluno2);
+    imprimir_turma(turma);
+    buscar_aluno_sequencial(turma, 2);
+    buscar_aluno_binaria(turma, 2);
+    liberar_turma(&turma);
+
+    return 0;
+}
