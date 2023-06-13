b6d553820008b3d33618f0933280fdcc377ca7f1
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 21:06:42 -0300

    Update main.c

--- Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Aluno/main.c
+++ Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Aluno/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "aluno.h"
 
+int main() {
+    
+    ALUNO aluno1 = criar_aluno(1, "João", 20, 8.5);
+    ALUNO aluno2 = criar_aluno(2, "Maria", 22, 9.0);
+    imprimir_aluno(aluno1);
+    printf("\n");
+    imprimir_aluno(aluno2); 
+    
+    return 0;
+}
