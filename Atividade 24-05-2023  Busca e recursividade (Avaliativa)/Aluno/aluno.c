5d33a6ed12948f3e6305a13870bbed9341da998b
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 21:00:51 -0300

    Update aluno.c

--- Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Aluno/aluno.c
+++ Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Aluno/aluno.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "aluno.h"
 
+ALUNO criar_aluno(int codigo, const char* nome, int idade, float coeficiente) {
+    ALUNO aluno;
+    aluno.codigo = codigo;
+    strcpy(aluno.nome, nome);
+    aluno.idade = idade;
+    aluno.coeficiente = coeficiente;
+    return aluno;
+}
+
+void imprimir_aluno(ALUNO aluno) {
+    printf("Código: %d\n", aluno.codigo);
+    printf("Nome: %s\n", aluno.nome);
+    printf("Idade: %d\n", aluno.idade);
+    printf("Coeficiente: %.2f\n", aluno.coeficiente);
+}
