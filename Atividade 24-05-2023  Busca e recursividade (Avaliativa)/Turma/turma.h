24c638fac4f67380014be00d6b6476750134bf25
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-12 21:13:05 -0300

    Update turma.h

--- Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Turma/turma.h
+++ Atividade 24-05-2023  Busca e recursividade (Avaliativa)/Turma/turma.h
@@ -1,1 +1,18 @@
+#ifndef TURMA_H
+#define TURMA_H
 
+#include "aluno.h"
+
+typedef struct {
+    ALUNO* alunos;
+    int quantidade;
+} TURMA;
+
+TURMA criar_turma(int quantidade);
+void liberar_turma(TURMA* turma);
+void inserir_aluno(TURMA* turma, ALUNO aluno);
+void buscar_aluno_sequencial(TURMA turma, int codigo);
+void buscar_aluno_binaria(TURMA turma, int codigo);
+void imprimir_turma(TURMA turma);
+
+#endif
