3dd6a5519523ad9d4b0f7f7d73a1f03bbd3cadf0
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-13 14:31:40 -0300

    Update turma_alunos.h

--- Atividade (avaliativa) 02-06-2023/turma_alunos.h
+++ Atividade (avaliativa) 02-06-2023/turma_alunos.h
@@ -1,1 +1,25 @@
+#ifndef TURMA_ALUNOS_H
+#define TURMA_ALUNOS_H
 
+#define MAX_ALUNOS 100
+
+typedef struct {
+    int matricula;
+    char nome[50];
+    int idade;
+} Aluno;
+
+typedef struct {
+    Aluno elementos[MAX_ALUNOS];
+    int quantidade;
+} ListaAlunos;
+
+void inicializarLista(ListaAlunos* lista);
+void liberarLista(ListaAlunos* lista);
+int quantidadeElementos(ListaAlunos* lista);
+int listaCheia(ListaAlunos* lista);
+int listaVazia(ListaAlunos* lista);
+void exibirLista(ListaAlunos* lista);
+void inserirElemento(ListaAlunos* lista, Aluno aluno);
+void excluirElemento(ListaAlunos* lista, int matricula);
+void alterarMatricula(ListaAlunos* lista, int matricula, int novaMatricula);
