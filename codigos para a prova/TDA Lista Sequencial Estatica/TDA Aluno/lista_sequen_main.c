79336cbe1e1e909fbcf69889d67280f41ec57c67
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-06-16 12:54:44 -0400

    Add files via upload

---
+++ codigos para a prova/TDA Lista Sequencial Estatica/TDA Aluno/lista_sequen_main.c
@@ -0,0 +1,12 @@
+#include "lista_sequencial.h"
+
+int main(){
+    Lista *li;
+    li = cria_lista();
+    int x = tamanho_lista(li)
+    int insere_lista_final(li, dados_aluno);
+    libera_lista(li);
+	return 0;
+}
+
+ 
---
+++ codigos para a prova/TDA Lista Sequencial Estatica/TDA Aluno/lista_sequencial.h
@@ -0,0 +1,16 @@
+#ifndef LISTA_SEQUENCIAL_H
+#define LISTA_SEQUENCIAL_H
+#include <stdio.h>
+#include <stdlib.h>
+  
+  
+  struct aluno {
+  	int codigo;
+  	char nome[20];
+  	int idade;
+  	int num_filhos;
+  };
+  
+  typedef struct lista Lista;
+  
+#endif
