e51845c10d70ed63689f215ea2bbbb24d9cbd0d4
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-07-30 11:21:18 -0300

    Create main.c

---
+++ Atividade 28-07-2023 Arvores binarias e expressões matemáticas/main.c
@@ -0,0 +1,40 @@
+int main() {
+    // Testando a inserção de elementos na árvore binária
+    Node* root = NULL;
+    insert(&root, '2');
+    insert(&root, '4');
+    insert(&root, '1');
+    insert(&root, '0');
+    insert(&root, '3');
+    insert(&root, '2');
+    insert(&root, '9');
+    insert(&root, '3');
+    insert(&root, '5');
+
+    // Imprimindo a árvore em diferentes ordens
+    printf("Infixa: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Posfixa: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    printf("Prefixa: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    // Convertendo uma expressão infixa para a árvore binária
+    char expression[] = "a*((b-c)/d)";
+    Node* rootFromInfix = infixToBinaryTree(expression);
+
+    // Calculando o valor da expressão aritmética
+    int result = evaluateExpression(rootFromInfix);
+    printf("Resultado: %d\n", result);
+
+    // Liberando a memória alocada para as árvores
+    freeTree(root);
+    freeTree(rootFromInfix);
+
+    return 0;
+}
