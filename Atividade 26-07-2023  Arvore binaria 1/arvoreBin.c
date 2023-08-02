0987838c0ec6dc913674158b897b223a16b734fa
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-08-01 22:58:52 -0300

    Update arvoreBin.c

--- Atividade 26-07-2023  Arvore binaria 1/arvoreBin.c
+++ Atividade 26-07-2023  Arvore binaria 1/arvoreBin.c
@@ -16,10 +16,11 @@ NO* criaNo(int valor){
 }
 
 void insereNo(ARVORE* arv, NO* raiz, int valor){
-	if(raiz == NUL){
+	if(raiz == NULL){
 		NO* no = criaNo(valor);
 		arv->raiz = no;
-		arv->quant++;
+		arv->quant = arv->quant + 1;
+		return;
 	}
 	if(raiz->valor > valor){
 		
@@ -28,16 +29,16 @@ void insereNo(ARVORE* arv, NO* raiz, int valor){
 		}
 		NO* no = criaNo(valor);
 		raiz->esquerda = no;
-		arv->quant++;
+		arv->quant = arv->quant + 1;
 	}
-	else if(raiz->direita <= valor){
+	else if(raiz->valor <= valor){
 		
 		if(raiz->direita != NULL){
 			return insereNo(arv, raiz->direita, valor);
 		}
 		NO* no = criaNo(valor);
 		raiz->direita = no;
-		arv->quant++;
+		arv->quant = arv->quant + 1;
 	}
 }
 
@@ -54,4 +55,44 @@ NO* buscaNo(NO* raiz, int valor){
 	
 }
 
+void imprimeArvore(NO* raiz){
+	if (raiz == NULL){
+		printf("Arvore vazia");
+		return;
+	}
+	 printf("Raiz: %d\n", raiz->valor);
+    printf("=> Esquerda: %d\n", raiz->valor);
+    imprimeArvore(raiz->esquerda);
+    printf("=> Direita: %d\n", raiz->valor);
+    imprimeArvore(raiz->direita);
+}
 
+void numeroDeNos(ARVORE* arv){
+	if(arv->quant == 0){
+		printf("Arvore vazia");
+		return;
+	}
+	int numero = arv->quant;
+	printf("\nNumero de elementos na árvore: %d\n", numero);
+}
+
+void preOrdem(NO* raiz){
+	if(raiz == NULL) return;
+	printf("\n%d", raiz->valor);
+	preOrdem(raiz->esquerda);
+	preOrdem(raiz->direita);
+}
+
+void posOrdem(NO* raiz){
+	if(raiz == NULL) return;
+	posOrdem(raiz->esquerda);
+	posOrdem(raiz->direita);
+	printf("\n%d", raiz->valor);
+}
+
+void inOrdem(NO* raiz){
+	if(raiz == NULL) return;
+	inOrdem(raiz->esquerda);
+	printf("\n%d", raiz->valor);
+	inOrdem(raiz->direita);
+}
